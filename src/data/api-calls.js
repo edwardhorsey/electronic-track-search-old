7a53ffd99053039af5fedf7b5af72f2dbf6b501e
Edward Horsey <edwardhorsey@me.com>
2020-07-17 09:08:46 +0100

    fix commas

--- src/data/api-calls.js
+++ src/data/api-calls.js
@@ -15,8 +15,8 @@ const uniqByProp_map = prop => arr =>
   Array.from(
     arr.reduce(
         (acc, item) => (
-          item && item[prop] && acc.set(item[prop], item),
-          acc
+          (item && item[prop] && acc.set(item[prop], item),
+          acc)
         ), // using map (preserves ordering)
         new Map()
       ).values()
