7993242072d63e82309111e7280efd84102126ba
Edward Horsey <edwardhorsey@me.com>
2020-07-22 18:38:25 +0100

    Fixed a bug when returning undefined soundcloud results. Add margin to spinner

--- src/component/Youtube/Youtube.jsx
+++ src/component/Youtube/Youtube.jsx
@@ -5,7 +5,7 @@ const Spinner = require('react-spinkit');
 
 class Youtube extends Component {
 
-  render = () => this.props.data === 'spinner' ? <Spinner name='three-bounce' color='dark-grey' /> : <ReactPlayer width={'100%'} url={this.url} />;
+  render = () => this.props.data === 'spinner' ? <Spinner name='three-bounce' color='dark-grey' style={{marginTop: '20px'}} /> : <ReactPlayer width={'100%'} url={this.url} />;
 
   get url() {
     return `https://www.youtube.com/watch?v=${this.props.data}`
--- src/container/SoundcloudContainer/SoundcloudContainer.jsx
+++ src/container/SoundcloudContainer/SoundcloudContainer.jsx
@@ -8,14 +8,14 @@ import { scLinks } from "../../data/import-data"
 class SoundcloudContainer extends Component {
 
   cleanData = arr => {
+    console.log(arr)
     let filteredarr = arr.filter(el => el.url)
     let uniq = uniqueByUrl(filteredarr)
     return uniq
   }
 
   render() {
-
-    const cleanedData = this.cleanData(this.props.data);
+    const cleanedData = this.props.data ? this.cleanData(this.props.data) : [];
     const players = cleanedData.length < 1 ? scLinks : cleanedData;
 
     return (
